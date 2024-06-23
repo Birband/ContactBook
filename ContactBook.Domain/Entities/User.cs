1f703b16c357bc156ed506de3162d5fc1be9f537
Birband <maciejsztramski@gmail.com>
2024-06-23 13:23:33 +0200

    feat: Add User entity class

---
+++ ContactBook.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace ContactBook.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
