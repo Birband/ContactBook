fb01fdde4698c46a627b0872bdfa949f9ff33ee5
Birband <maciejsztramski@gmail.com>
2024-06-24 19:40:19 +0200

    feat: Add initial data for categories and subcategories
    - Adds initial data for categories and subcategories in the ContactBookContext.cs file.
    - This data will be seeded to the database when the application starts.

--- ContactBook.Infrastructure/Persistence/ContactBookContext.cs
+++ ContactBook.Infrastructure/Persistence/ContactBookContext.cs
@@ -20,5 +20,26 @@ public class ContactBookDbContext : DbContext
             .WithOne(s => s.Category)
             .HasForeignKey(s => s.CategoryId)
             .OnDelete(DeleteBehavior.Cascade);
+        
+        List<Domain.Entities.Category> users = new()
+        {
+            new Domain.Entities.Category { Id = Guid.Parse("b6f0f09c-e503-4804-84cb-5bce1139e0d7"), Name = "Business" },
+            new Domain.Entities.Category { Id = Guid.Parse("90ec091c-95fc-454d-9924-cf8e7fd2c217"), Name = "Private" },
+            new Domain.Entities.Category { Id = Guid.Parse("9a9c5d7d-d124-45dc-9a59-ef6486cd42fc"), Name = "Other" }
+        };
+        // Add 3 categories to the database
+        modelBuilder.Entity<Domain.Entities.Category>().HasData(
+            users[0],
+            users[1],
+            users[2]
+        );
+
+        // Add 3 subcategories to the database
+        modelBuilder.Entity<Domain.Entities.Subcategory>().HasData(
+            new Domain.Entities.Subcategory { Id = Guid.Parse("9a71bcab-87cf-44b1-aa44-82f58ef0cb6f"), Name = "Business", CategoryId = Guid.Parse("b6f0f09c-e503-4804-84cb-5bce1139e0d7") },
+            new Domain.Entities.Subcategory { Id = Guid.Parse("20f817b0-cfaa-43a0-b634-2242f82964f4"), Name = "Client", CategoryId = Guid.Parse("b6f0f09c-e503-4804-84cb-5bce1139e0d7") },
+            new Domain.Entities.Subcategory { Id = Guid.Parse("5c861730-2b82-4012-ac6f-cfff4dcbaafc"), Name = "Home", CategoryId = Guid.Parse("9a9c5d7d-d124-45dc-9a59-ef6486cd42fc") },
+            new Domain.Entities.Subcategory { Id = Guid.Parse("4bce6ac3-b6ed-488b-a759-64cafef966f7"), Name = "Mobile", CategoryId = Guid.Parse("9a9c5d7d-d124-45dc-9a59-ef6486cd42fc") }
+        );
     }
 }
