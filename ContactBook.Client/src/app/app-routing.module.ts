9a94f19e7f7cb5504c7550929246c9d2fbbd6dc5
Birband <maciejsztramski@gmail.com>
2024-06-25 12:27:31 +0200

    feat: Add route for displaying contact details
    The code changes add a new route in the app-routing.module.ts file to handle displaying contact details. This route is mapped to the ContactDetailComponent.

--- ContactBook.Client/src/app/app-routing.module.ts
+++ ContactBook.Client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 const routes: Routes = [
   { path: 'contacts', component: ContactsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'contact/:email', component: ContactDetailComponent },
   { path: '', redirectTo: '/contacts', pathMatch: 'full' }
 ];
 
